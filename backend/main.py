1aab7cc94a0933eeea0419bed00ac8aacc594900
Sean San <seanksan@gmail.com>
2025-11-24 20:48:20 -0800

    Set up the pydantic base models
    Transfering songs between both spotify and youtube now have a pydantic
    class set up. This will help to validate data and ensure that the API
    knows what to expect.
    
    The parameters may change as the project is built up more.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"FastAPI is running"}
+
+class YoutubeToSpotify(Event):
+    name: str
+    youtube_playlist_link: str
+
+class SpotifyToYoutube(Evnet):
+    name: str
+    username: str
+    password: str
+    spotify_playlist_link: str
