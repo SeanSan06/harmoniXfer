7b352ac0a54fe8b4d834608e491e84934b9c3fcf
Sean San <seanksan@gmail.com>
2025-11-24 22:20:15 -0800

    Backend serves up the home page

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,28 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI                 # Backend Framework
+from fastapi.responses import FileResponse  # Send file to broswer
+from fastapi.staticfiles import StaticFiles # Serve folder
+from pydantic import BaseModel              # Helps type check
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"FastAPI is running"}
-
-class YoutubeToSpotify(Event):
+class YoutubeToSpotify(BaseModel):
     name: str
     youtube_playlist_link: str
 
-class SpotifyToYoutube(Evnet):
+class SpotifyToYoutube(BaseModel):
     name: str
     username: str
     password: str
     spotify_playlist_link: str
+
+# End points
+# @app.get("/")
+# def home():
+#     return {"FastAPI is running"}
+
+# Serve Webpages
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+@app.get("/")
+def serve_home():
+    return FileResponse("frontend/html/index.html")
