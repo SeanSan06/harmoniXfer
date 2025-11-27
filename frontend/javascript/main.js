ece4a0ccf30edda4185b16a0a5a1fe929deafe5b
Sean San <seanksan@gmail.com>
2025-11-27 01:17:14 -0800

    Allow user to input info to txt box and capture it
    One of the cards can now capture user input. The user input is the ID of
    a youtube playlist they want to transfer songs from.
    
    Note: This is not connected to the backend yet. Also, invalid inputs
    will need to be error checked.

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -8,9 +8,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// Custom sleep function
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+// Home page animations upon loading
 window.addEventListener("load", () => __awaiter(void 0, void 0, void 0, function* () {
     console.log("Testing testing");
     yield sleep(1600);
@@ -21,4 +23,19 @@ window.addEventListener("load", () => __awaiter(void 0, void 0, void 0, function
     yield sleep(900);
     document.querySelector("#image").classList.add("appear-fade-in");
 }));
+// Transfer button(gets titles of YouTube videos for now)
+window.addEventListener("DOMContentLoaded", () => {
+    function qs(selector) {
+        const el = document.querySelector(selector);
+        if (!el)
+            throw new Error(`Element not found: ${selector}`);
+        return el;
+    }
+    const inputTextBox = qs("#youtube_playlist_id_1");
+    const button = qs("#youtube_to_spotify_button");
+    button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
+        const text = inputTextBox.value;
+        console.log("user tpyed in box", text);
+    });
+});
 //# sourceMappingURL=main.js.map
--- frontend/ts/main.ts
+++ frontend/ts/main.ts
@@ -1,8 +1,9 @@
-
+// Custom sleep function
 function sleep(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// Home page animations upon loading
 window.addEventListener("load", async () => {
     console.log("Testing testing");
     await sleep(1600);
@@ -14,3 +15,22 @@ window.addEventListener("load", async () => {
     await sleep(900);
     document.querySelector("#image")!.classList.add("appear-fade-in");
 });
+
+// Transfer button(gets titles of YouTube videos for now)
+window.addEventListener("DOMContentLoaded", () => {
+    function qs<T extends HTMLElement>(selector: string): T {
+        const el = document.querySelector(selector);
+        if (!el) throw new Error(`Element not found: ${selector}`);
+        return el as T;
+    }
+
+    const inputTextBox = qs<HTMLInputElement>("#youtube_playlist_id_1")!;
+    const button = qs<HTMLButtonElement>("#youtube_to_spotify_button")!;
+
+    button?.addEventListener("click", () => {
+        const text = inputTextBox.value;
+        console.log("user tpyed in box", text);
+    });
+});
+
+
