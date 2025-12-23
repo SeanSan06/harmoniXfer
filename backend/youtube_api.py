695d9ed3487eecbd168f0cf6b02b4db4331c495b
Sean San <seanksan@gmail.com>
2025-12-23 00:01:33 -0800

    Add a function that helps neaten up YT titles
    Many YouTube videos of songs have extra words in them like, Official
    Music Video. This function will help to parse those extra words out
    which makes Spotifys API more accurate in finding the correct song.

--- backend/youtube_api.py
+++ backend/youtube_api.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
+import re
+from typing import List
 
 # Get Youtube API key
 load_dotenv()
@@ -8,7 +10,7 @@ API_KEY = os.getenv("YOUTUBE_API_KEY")
 YOUTUBE = build("youtube", "v3", developerKey=API_KEY)
 
 # Get Youtube video titles from a selected playlist
-def get_playlist_videos_title(playlist_id):
+def get_playlist_videos_title(playlist_id: List[str]) -> List[str]:
     videos = []
     next_page = None
 
@@ -30,3 +32,43 @@ def get_playlist_videos_title(playlist_id):
             break
 
     return videos
+
+# Uses regex to clean titles
+def parse_video_titles(song_titles_list: List[str]) -> List[str]:
+    junk_phrases = [
+        "official video",
+        "official music video",
+        "official hd video",
+        "official hd music",
+        "official hd music video",
+        "official music",
+        "official audio",
+        "official lyric",
+        "official lyrics",
+        "official lyric video",
+        "audio",
+        "audios",
+        "lyric",
+        "lyrics",
+        "lyric video",
+        "lyric audio",
+        "audio music",
+        "official audio",
+        "music audio",
+        "audio visualizer",
+        "4k video",
+        "4k music video",
+        "official 4k music video",
+    ]
+
+    cleaned_titles = []
+
+    def clean_title(song_title: str) -> str:
+        title = re.sub("\/()[]^*", "", title)
+
+        for phrase in junk_phrases:
+            title = re.sub(phrase, "", title, flags = re.IGNORECASE)
+
+        return " ".join(title.split())
+    
+    return [clean_title(song_title) for song_title in song_titles_list]
